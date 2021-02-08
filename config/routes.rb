7613ef64b2adaeefd67275660757e9c9ff3accf9
YutaYamaki <8makicam@gmail.com>
2021-02-08 08:09:53 +0000

    [add]投稿一覧

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show' => "users#show"
+  devise_for :users
+  root to: 'homes#index'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
 end
