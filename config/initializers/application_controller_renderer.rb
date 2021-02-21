bd71cc6b65ccd0f56f24089f65b5084e9ad870ee
YutaYamaki <8makicam@gmail.com>
2021-02-21 02:46:48 +0000

    viewfile内のcolumn名変更

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7b0581b92205b87a7814a3b4cdc1977610cdb2f2266673bb0b76614623ad85a9bae26a8b89917f1375f444c8ddd92ecd965319543aa19d1007dee800b322b4fe'
